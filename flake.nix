99b0b96e0417a401406a7ee044755d47bdedee20
Austin Platt <austinplatt@gmail.com>
2024-01-02 15:23:41 +0000

    use lttoolbox from nixpkgs unstable

--- flake.nix
+++ flake.nix
@@ -1,38 +1,13 @@
 {
   description = "verb conjugation";
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = github:numtide/flake-utils;
   };
   outputs = { self, nixpkgs, flake-utils }:
     with flake-utils.lib; eachSystem allSystems (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        lttoolbox = pkgs.stdenv.mkDerivation {
-          src = pkgs.fetchFromGitHub {
-            owner = "apertium";
-            repo = "lttoolbox";
-            rev = "3d82cd337e81877f20f7ad79ea398ad184fae5ac";
-            sha256 = "sha256-3lHXKtwQSrMGQEGOGr27e3kB2qKkTFZcEzeAnIm89Rg=";
-          };
-          name = "lttoolbox";
-          buildInputs = [
-            pkgs.autoconf
-            pkgs.automake
-            pkgs.libtool
-            pkgs.libxml2
-            pkgs.libxml2.dev
-            pkgs.icu
-            pkgs.pkg-config
-            pkgs.utf8cpp
-          ];
-          buildFlags = [
-            "CPPFLAGS=-I${pkgs.utf8cpp}/include/utf8cpp"
-          ];
-          configurePhase = ''
-            ./autogen.sh --prefix $out
-          '';
-        };
       in
       {
         # defaultPackage = packages.document;
@@ -45,7 +20,7 @@
             pkgs.yq-go
             pkgs.jq
             pkgs.ffmpeg
-            lttoolbox
+            pkgs.lttoolbox
           ];
           shellHook = ''
             export PATH=$PATH:./node_modules/.bin/
